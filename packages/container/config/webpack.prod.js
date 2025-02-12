5f7e125d1896f6a4d9e00d43b8a54cc5ddeacef5
Subhradeep Mukherjee <subhradeep1007@gmail.com>
2025-02-12 19:51:42 +0530

    minor changes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,7 @@ const domain = process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js',
+    filename: 'container/latest/[name].[contenthash].js',
   },
   plugins: [
     new ModuleFederationPlugin({
