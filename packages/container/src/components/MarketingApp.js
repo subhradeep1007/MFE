1ba25ef8a545cdd9e6025f07eca71d42102345fe
Subhradeep Mukherjee <subhradeep1007@gmail.com>
2025-02-13 19:40:23 +0530

    minor changes

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -3,7 +3,7 @@ import React, { useRef, useEffect } from 'react';
 
 export default () => {
   const ref = useRef(null);
-  console.log('hello moto hello')
+  console.log('hello moto hello hello')
   useEffect(() => {
     mount(ref.current);
   });
